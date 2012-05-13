32ece7119bce4bb91eaa35b31d7ed944cafa0b94
Simon Whitaker <simon@goosoftware.co.uk>
2012-05-13 18:24:22 +0100

    Add zsh autocompletion file

---
+++ gibo-completion.zsh
@@ -0,0 +1,28 @@
+#!/zsh
+# 
+# Zsh completion for gibo
+# 
+# INSTALLATION
+# 
+# First install gibo from 
+# https://github.com/simonwhitaker/gitignore-boilerplates
+# 
+# Make sure autocompletion is enabled in your shell, typically
+# by adding this to your .zshrc:
+# 
+#     autoload -U compinit && compinit
+# 
+# Then copy this file somewhere (e.g. ~/.gibo-completion.zsh) and put the
+# following in your .zshrc:
+# 
+#     source ~/.gibo-completion.zsh
+# 
+# CREDITS
+# 
+# Written by Simon Whitaker <simon@goosoftware.co.uk>
+
+_gibo()
+{
+    reply=($( find $HOME/.gitignore-boilerplates -name "*.gitignore" -exec basename \{\} .gitignore \; ))
+}
+compctl -K _gibo gibo
