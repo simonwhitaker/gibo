bad8be3d5a093beb2faf6d96b252699b1ecf61e8
Simon Whitaker <sw@netcetera.org>
2014-12-25 22:38:39 +0000

    Update version to 1.0.4

--- gibo.bat
+++ gibo.bat
@@ -13,7 +13,7 @@ goto :setup
 
 
 :version
-    echo %basename% 1.0.3 by Simon Whitaker ^<sw@netcetera.org^>
+    echo %basename% 1.0.4 by Simon Whitaker ^<sw@netcetera.org^>
     echo https://github.com/simonwhitaker/gitignore-boilerplates
     goto :eof
 
