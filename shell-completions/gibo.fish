3a5ab931c26938476ff1b092cb16081f70ae4219
Simon Whitaker <sw@netcetera.org>
2018-06-12 17:18:35 +0100

    [v2] Update fish tab completion script
    Closes #29

--- shell-completions/gibo.fish
+++ shell-completions/gibo.fish
@@ -11,6 +11,19 @@
 #
 # Written by Sebastian Schulz <https://github.com/yilazius>
 
+function __fish_using_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -eq (count $argv) ]
+        for i in (seq (count $argv))
+            if [ $cmd[$i] != $argv[$i] ]
+                return 1
+            end
+        end
+        return 0
+    end
+    return 1
+end
+
 function gibocompletionlist
   set gitignores (ls ~/.gitignore-boilerplates/**.gitignore)
   set completions
@@ -20,4 +33,9 @@ function gibocompletionlist
   echo $completions
 end
 
-complete -c gibo -f -a (gibocompletionlist)
+complete -c gibo -n "__fish_using_command gibo" -f -a "dump" -d 'Dump one or more boilerplates to STDOUT'
+complete -c gibo -n "__fish_using_command gibo" -f -a "help" -d 'Show help information'
+complete -c gibo -n "__fish_using_command gibo" -f -a "list" -d 'Show the list of available boilerplates'
+complete -c gibo -n "__fish_using_command gibo" -f -a "update" -d 'Update the list of available boilerplates'
+complete -c gibo -n "__fish_using_command gibo" -f -a "version" -d 'Show the current version of gibo installed'
+complete -c gibo -n "__fish_using_command gibo dump" -f -a (gibocompletionlist)
