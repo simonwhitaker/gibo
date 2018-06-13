0f92d414829c1cf205350c019b2835422e926bc8
Simon Whitaker <sw@netcetera.org>
2018-06-13 05:46:29 -0700

    Update gibo.bat to v2.0
    Closes #26

--- gibo.bat
+++ gibo.bat
@@ -13,7 +13,7 @@ goto :setup
 
 
 :version
-    echo %basename% 1.0.6 by Simon Whitaker ^<sw@netcetera.org^>
+    echo %basename% 2.0.0 by Simon Whitaker ^<sw@netcetera.org^>
     echo https://github.com/simonwhitaker/gitignore-boilerplates
     goto :eof
 
@@ -30,10 +30,10 @@ goto :setup
     echo     %basename% Python TextMate ^>^> .gitignore
     echo.
     echo Options:
-    echo     -l, --list          List available boilerplates
-    echo     -u, --upgrade       Upgrade list of available boilerplates
-    echo     -h, --help          Display this help text
-    echo     -v, --version       Display current script version
+    echo     list          List available boilerplates
+    echo     update        Update list of available boilerplates
+    echo     help          Display this help text
+    echo     version       Display current script version
 
     goto :eof
 
@@ -42,10 +42,7 @@ goto :setup
     set "basename=%~n0"
     set "baseext=%~x0"
     set "basepath=%~dp0"
-
-    set "__quiet="
-    set "__verbose="
-    set "__pause="
+    set dumping=0
 
     set "remote_repo=https://github.com/github/gitignore.git"
     rem set "local_repo=%UserProfile%\.gitignore-boilerplates"
@@ -57,68 +54,28 @@ goto :setup
 :parse
     rem Parse comand-line options.
 
-    if "%~1"=="" goto :main
-
-    if not defined __quiet (
-        if defined __verbose echo parse^('%~1'^)
-    )
+    if "%~1"=="" exit /B 0
 
     set a=%~1
-    :: Replace all '--' with '-' (for options)
-    set a=%a:--=-%
-    :: Get the first 2 characters of `a`
-    set a2=%a:~0,2%
 
     rem The batch file's equivalent 'Sanity check' is that any
     rem options (-, --, and / for Windows) are executed then
     rem the batch file exits, ignoring any other commands, such
     rem as Python
 
-    if /i "%a%"=="/?" call :usage && endlocal && exit /B 0
-    if /i "%a2%"=="-h" call :usage && endlocal && exit /B 0
+    if "%a%"=="help" call :usage && exit /B 0
+    if "%a%"=="version" call :version && exit /B 0
+    if "%a%"=="list" call :list && exit /B 0
+    if "%a%"=="update" call :update && exit /B 0
+    if "%a%"=="dump" set dumping=1 && shift && goto :parse
 
-    if /i "%a2%"=="-q" set "__quiet=1" && shift && goto :parse
-    if /i "%a2%"=="/q" set "__quiet=1" && shift && goto :parse
-
-    if /i "%a%"=="-verbose" set "__verbose=1" && shift && goto :parse
-    if /i "%a%"=="/verbose" set "__verbose=1" && shift && goto :parse
-
-    if /i "%a2%"=="-p" set "__pause=1" && shift && goto :parse
-    if /i "%a2%"=="/p" set "__pause=1" && shift && goto :parse
-
-    rem rem Takes the next argument also (calls shift twice)..
-    rem if /i "%a2%"=="-repo" set "remote_repo=%~2" && shift && shift && goto :parse
-    rem if /i "%a2%"=="/repo" set "remote_repo=%~2" && shift && shift && goto :parse
-
-    if /i "%a2%"=="-v" call :version && exit /B 0
-    if /i "%a2%"=="/v" call :version && exit /B 0
-
-    if /i "%a2%"=="-l" call :list && exit /B 0
-    if /i "%a2%"=="/l" call :list && exit /B 0
-
-    if /i "%a2%"=="-u" call :upgrade && exit /B 0
-    if /i "%a2%"=="/u" call :upgrade && exit /B 0
-
-    call :dump "%a%"
-
-    shift
-    goto :parse
-
-:main
-    if not defined __quiet (
-        if defined __verbose echo main^(^)
-    )
-    if defined __pause pause
+    if %dumping% equ 1 call :dump "%a%" && shift && goto :parse
 
 :end
     @endlocal && exit /B 0
 
 
 :clone
-    if not defined __quiet (
-        if defined __verbose echo clone^(^)
-    )
-
     if not "%~1"=="--silently" (
         set "opt=-q"
         if not defined __quiet (
@@ -132,19 +89,11 @@ goto :setup
 
 
 :init
-    if not defined __quiet (
-        if defined __verbose echo init^(^)
-    )
-
     if not exist "%local_repo%\.git" call :clone "%~1"
     goto :eof
 
 
 :list
-    if not defined __quiet (
-        if defined __verbose echo list^(^)
-    )
-
     call :init
 
     echo === Languages ===
@@ -163,11 +112,7 @@ goto :setup
     goto :eof
 
 
-:upgrade
-    if not defined __quiet (
-        if defined __verbose echo upgrade^(^)
-    )
-
+:update
     if not exist "%local_repo%\.git" call :clone && goto :eof
 
     pushd "%local_repo%"
@@ -178,10 +123,6 @@ goto :setup
 
 
 :dump
-    if not defined __quiet (
-        if defined __verbose echo dump^('%~1'^)
-    )
-
     call :init --silently
 
     set "language_file=%local_repo%\%~1.gitignore"
