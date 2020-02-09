62dcb05089d1c4b1bc9c611ea43a4155252afeaa
Simon Whitaker <sw@netcetera.org>
2020-02-09 20:52:41 +0000

    Add tests (#48)
    * Add tests
    
    * Silence redundant warnings in test output

---
+++ tests/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export GIBO_BOILERPLATES=$(dirname $0)/mock-boilerplates
+gibo=$(dirname $0)/../gibo
+known=Foo
+unknown=Bar
+
+fail() {
+    echo $1
+    exit 1
+}
+
+# Create a dummy .git directory inside $GIBO_BOILERPLATES to placate gibo's
+# clone function.
+(cd $GIBO_BOILERPLATES && mkdir -p .git)
+
+# Calling gibo without subcommand exits with non-zero exit status
+$gibo >/dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+    fail "Got zero exit status when run without subcommand"
+fi
+
+# It fails when the boilerplate file doesn't exist
+$gibo dump $unknown >/dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+    fail "Got zero exit status for unknown boilerplate"
+fi
+
+# It succeeds when the boilerplate file exists
+$gibo dump $known >/dev/null
+
+if [[ $? -ne 0 ]]; then
+    fail "Got non-zero exit status for known boilerplate"
+fi
+
+# `gibo dump Foo` outputs 7 lines
+expected=7
+lines=$($gibo dump $known | wc -l)
+if [[ $lines -ne $expected ]]; then
+    fail "Expected $expected lines in output of 'gibo dump $known', got $lines"
+fi
