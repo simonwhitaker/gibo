92a2ffc7f813bf7bd3a0eea46c0eecb39d7f8e88
Simon Whitaker <sw@netcetera.org>
2020-11-01 21:15:39 +0000

    Add test for dumping multiple boilerplates

--- tests/test.sh
+++ tests/test.sh
@@ -3,8 +3,6 @@
 GIBO_BOILERPLATES=$(dirname "$0")/mock-boilerplates
 export GIBO_BOILERPLATES
 gibo=$(dirname "$0")/../gibo
-known=Foo
-unknown=Bar
 
 fail() {
     echo "$1"
@@ -21,18 +19,25 @@ if $gibo >/dev/null 2>&1; then
 fi
 
 # It fails when the boilerplate file doesn't exist
-if $gibo dump $unknown >/dev/null 2>&1; then
+if $gibo dump UnknownBoilerplate >/dev/null 2>&1; then
     fail "Got successful (zero) exit status for unknown boilerplate"
 fi
 
 # It succeeds when the boilerplate file exists
-if ! $gibo dump $known >/dev/null; then
+if ! $gibo dump Foo >/dev/null; then
     fail "Got unsuccessful (non-zero) exit status for known boilerplate"
 fi
 
 # `gibo dump Foo` outputs 7 lines
 expected=7
-lines=$($gibo dump $known | wc -l)
+lines=$($gibo dump Foo | wc -l)
 if [[ $lines -ne $expected ]]; then
-    fail "Expected $expected lines in output of 'gibo dump $known', got $lines"
+    fail "Expected $expected lines in output of 'gibo dump Foo', got $lines"
+fi
+
+# `gibo dump Foo Bar` outputs 14 lines
+expected=14
+lines=$($gibo dump Foo Bar | wc -l)
+if [[ $lines -ne $expected ]]; then
+    fail "Expected $expected lines in output of 'gibo dump Foo Bar', got $lines"
 fi
