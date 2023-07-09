53111f54843baaec130e4577fc7ee5318eb93ab2
Simon Whitaker <sw@netcetera.org>
2023-07-09 21:48:41 +0100

    Populate version info from repo metadata

--- cmd/version.go
+++ cmd/version.go
@@ -2,14 +2,24 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+)
+
+// These vars are populated at build time via the `-ldflags` configured in
+// `.gorelease.yml`. More info at https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the current version number of gibo",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("gibo v2.0")
+		fmt.Printf("%v v%v, commit %v, built on %v", filepath.Base(os.Args[0]), version, commit, date)
 	},
 }
