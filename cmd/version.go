0b684ecf6ea67b365face898f3e6524340fa1ee7
Simon Whitaker <sw@netcetera.org>
2023-08-11 07:46:25 +0100

    Simplify version string

--- cmd/version.go
+++ cmd/version.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -12,8 +10,6 @@ import (
 // `.gorelease.yml`. More info at https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
 var (
 	version = "dev"
-	commit  = "none"
-	date    = "unknown"
 )
 
 func init() {
@@ -24,6 +20,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the current version number of gibo",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%v v%v, commit %v, built on %v\n", filepath.Base(os.Args[0]), version, commit, date)
+		fmt.Println(version)
 	},
 }
