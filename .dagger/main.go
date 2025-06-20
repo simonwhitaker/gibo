f01012c4bd8af6981c3c0c0eab40505d1efc655e
Simon Whitaker <sw@netcetera.org>
2025-06-20 22:28:14 +0100

    Add basic dagger config

---
+++ .dagger/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"dagger/gibo/internal/dagger"
+)
+
+type Gibo struct{}
+
+func (m *Gibo) BuildEnv(src *dagger.Directory) *dagger.Container {
+	return dag.Container().
+		From("golang:1.24.4-alpine").
+		WithDirectory("/app", src).
+		WithWorkdir("/app").
+		WithEnvVariable("CGO_ENABLED", "0").
+		// WithEnvVariable("GOOS", "darwin").
+		// WithEnvVariable("GOARCH", "arm64").
+		WithExec([]string{"go", "mod", "download"})
+}
+
+func (m *Gibo) Build(src *dagger.Directory) *dagger.File {
+	return m.BuildEnv(src).
+		// WithEnvVariable("GOOS", "darwin").
+		// WithEnvVariable("GOARCH", "arm64").
+		WithExec([]string{"go", "build", "-o", "gibo"}).
+		File("/app/gibo")
+}
+
+func (m *Gibo) Test(ctx context.Context,
+	src *dagger.Directory) (string, error) {
+	return m.BuildEnv(src).
+		WithExec([]string{"go", "test", "./..."}).
+		Stdout(ctx)
+}
