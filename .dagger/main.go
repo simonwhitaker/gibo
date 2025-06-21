e652fbf3c28c0fc167e0219d25dcc9c3de116a3c
Simon Whitaker <sw@netcetera.org>
2025-06-21 08:02:01 +0100

    Add optional os and arch args to Build

--- .dagger/main.go
+++ .dagger/main.go
@@ -7,35 +7,46 @@ import (
 
 type Gibo struct{}
 
-func (m *Gibo) BuildEnv(src *dagger.Directory) *dagger.Container {
-	return dag.Container().
+func (m *Gibo) BuildEnv(src *dagger.Directory,
+	// +optional
+	arch,
+	// +optional
+	os string) *dagger.Container {
+	container := dag.Container().
 		From("golang:1.24.4-alpine").
-		WithDirectory("/app", src).
+		WithMountedDirectory("/app", src).
 		WithWorkdir("/app").
-		WithEnvVariable("CGO_ENABLED", "0").
-		// WithEnvVariable("GOOS", "darwin").
-		// WithEnvVariable("GOARCH", "arm64").
-		WithExec([]string{"go", "mod", "download"})
+		WithEnvVariable("CGO_ENABLED", "0")
+
+	if arch != "" {
+		container = container.WithEnvVariable("GOARCH", arch)
+	}
+	if os != "" {
+		container = container.WithEnvVariable("GOOS", os)
+	}
+	return container.WithExec([]string{"go", "mod", "download"})
 }
 
-func (m *Gibo) Build(src *dagger.Directory) *dagger.File {
-	return m.BuildEnv(src).
-		// WithEnvVariable("GOOS", "darwin").
-		// WithEnvVariable("GOARCH", "arm64").
+func (m *Gibo) Build(src *dagger.Directory,
+	// +optional
+	arch,
+	// +optional
+	os string) *dagger.File {
+	return m.BuildEnv(src, arch, os).
 		WithExec([]string{"go", "build", "-o", "gibo"}).
 		File("/app/gibo")
 }
 
 func (m *Gibo) Test(ctx context.Context,
 	src *dagger.Directory) (string, error) {
-	return m.BuildEnv(src).
+	return m.BuildEnv(src, "", "").
 		WithExec([]string{"go", "test", "./..."}).
 		Stdout(ctx)
 }
 
 func (m *Gibo) Vet(ctx context.Context,
 	src *dagger.Directory) (string, error) {
-	return m.BuildEnv(src).
+	return m.BuildEnv(src, "", "").
 		WithExec([]string{"go", "vet", "./..."}).
 		Stdout(ctx)
 }
